761cec610d30926e499c236c6718fd26c42340c7
June Santos <91368271+NeoRootPain@users.noreply.github.com>
2025-08-10 01:05:24 -0300

    Add files via upload

---
+++ js/menugreen.js
@@ -0,0 +1,80 @@
+let MenuContent = `
+
+<div class='navigationcontainer'>
+
+<img src='./images/menus/navigationbargreen.png' width='226px' height='33px'>
+
+<a href='./index.html'>
+<div class='buttoncontainer'>
+<img src='./images/menus/homebuttonfaded.png' class='buttonfade'>
+<img src='./images/menus/homebutton.png' class='button'>
+<img src='./images/menus/homebuttonslide.png' class='buttonslide'>
+</div>
+</a>
+
+<a href='./blogmain.html'>
+<div class='buttoncontainer'>
+<img src='./images/menus/blogbuttonfaded.png' class='buttonfade'>
+<img src='./images/menus/blogbutton.png' class='button'>
+<img src='./images/menus/blogbuttonslide.png' class='buttonslide'>
+</div>
+</a>
+
+<img src='./images/menus/menuhandlebargreen.png' width='226px' height='13px'>
+</div>
+
+<div class='navigationcontainer'>
+
+<img src='./images/menus/partnersbargreen.png' width='226px' height='33px'>
+
+<a href='https://kaiakairos.net/'>
+<div class='buttoncontainer'>
+<img src='./images/menus/kaiakairosbuttonfaded.png' class='buttonfade'>
+<img src='./images/menus/kaiakairosbutton.png' class='button'>
+<img src='./images/menus/kaiakairosbuttonslide.png' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://staggernight.com/'>
+<div class='buttoncontainer'>
+<img src='./images/menus/staggernightbuttonfaded.png' class='buttonfade'>
+<img src='./images/menus/staggernightbutton.png' class='button'>
+<img src='./images/menus/staggernightbuttonslide.png' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://icantseehelp.neocities.org/'>
+<div class='buttoncontainer'>
+<img src='./images/menus/icantseehelpbuttonfaded.png' class='buttonfade'>
+<img src='./images/menus/icantseehelpbutton.png' class='button'>
+<img src='./images/menus/icantseehelpbuttonslide.png' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://sgxp.me/'>
+<div class='buttoncontainer'>
+<img src='./images/menus/sgxpbuttonfaded.png' class='buttonfade'>
+<img src='./images/menus/sgxpbutton.png' class='button'>
+<img src='./images/menus/sgxpbuttonslide.png' class='buttonslide'>
+</div>
+</a>
+
+<img src='./images/menus/menuhandlebargreen.png' width='226px' height='13px'>
+</div>
+
+`;
+document.querySelector('#menucontainer').insertAdjacentHTML('beforeend', MenuContent);
+
+document.querySelectorAll('.buttoncontainer').forEach(btn => {
+  btn.addEventListener('mouseover', () => {
+    let sound = new Audio('./sounds/buttonhover.ogg');
+    sound.play();
+  });
+});
+
+document.querySelectorAll('.buttoncontainer').forEach(btn => {
+  btn.addEventListener('mousedown', () => {
+    let sound = new Audio('./sounds/buttonselect.ogg');
+    sound.play();
+  });
+});
