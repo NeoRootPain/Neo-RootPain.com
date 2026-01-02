e332f67be5ba3fca7253d7e99fc391e5bc340ed4
June Santos <91368271+NeoRootPain@users.noreply.github.com>
2026-01-01 21:12:21 -0300

    Add files via upload

---
+++ js/menublack.js
@@ -0,0 +1,159 @@
+let MenuContent = `
+
+<!--Navigation-->
+
+<div class='navigationcontainer' style='margin-top:10px;'>
+
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/navigationbarblack.png?raw=true'>
+
+<a href='https://rootpain.com/index.html'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/homebuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/homebutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/homebuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://rootpain.com/blogmain.html'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/blogbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/blogbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/blogbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://rootpain.com/fanartmain.html'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/fanartbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/fanartbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/fanartbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='guestbook.html'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/guestbookbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/guestbookbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/guestbookbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://rootpain.com/changelog.html'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/changelogbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/changelogbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/changelogbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://rootpain.com/aboutme.html'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/aboutmebuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/aboutmebutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/aboutmebuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/menuhandlebarblack.png?raw=true'>
+</div>
+
+<!--Partners-->
+
+<div class='navigationcontainer'>
+
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/partnersbargreen.png?raw=true.png'>
+
+<a href='https://kaiakairos.net/'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/kaiakairosbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/kaiakairosbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/kaiakairosbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://staggernight.com/'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/staggernightbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/staggernightbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/staggernightbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://icantseehelp.neocities.org/'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/icantseehelpbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/icantseehelpbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/icantseehelpbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<a href='https://sgxp.me/'>
+<div class='buttoncontainer'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/sgxpbuttonfaded.png?raw=true' class='buttonfade'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/sgxpbutton.png?raw=true' class='button'>
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/sgxpbuttonslide.png?raw=true' class='buttonslide'>
+</div>
+</a>
+
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/menuhandlebarblack.png?raw=true'>
+</div>
+
+<!--Stamps-->
+
+<div class='navigationcontainer'>
+
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/stampsbarblack.png?raw=true.png'>
+
+<div class='stampscontainer'>
+<a href='https://icantseehelp.neocities.org/'>
+<img src='./images/stamps/CharlotteStamp2.gif' class='stamp'>
+</a>
+
+<a href='https://icantseehelp.neocities.org/'>
+<img src='./images/stamps/CharlotteStamp3.gif' class='stamp'>
+</a>
+
+<a href='https://www.youtube.com/watch?v=QPgkkMG9oq0' target='_blank'>
+<img src='./images/stamps/parappa.webp' class='stamp'>
+</a>
+
+<a href='https://www.youtube.com/watch?v=YiHEWozeAb8' target='_blank'>
+<img src='./images/stamps/lbp2.png' class='stamp'>
+</a>
+
+<a href='https://www.youtube.com/watch?v=eBD_6l_KL9k' target='_blank'>
+<img src='./images/stamps/postal2stamp.png' class='stamp'>
+</a>
+
+<a href='https://www.youtube.com/watch?v=JUv5n7M82mM' target='_blank'>
+<img src='./images/stamps/house.gif' class='stamp'>
+</a>
+
+<a href='https://www.youtube.com/watch?v=JLswtnD54l8' target='_blank'>
+<img src='./images/stamps/marie.gif' class='stamp'>
+</a>
+
+<a href='https://www.youtube.com/watch?v=JLswtnD54l8' target='_blank'>
+<img src='./images/stamps/callie.gif' class='stamp'>
+</a>
+</div>
+
+<img src='https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/menus/menuhandlebarblack.png?raw=true'>
+</div>
+`;
+
+document.querySelector('#menucontainer').insertAdjacentHTML('beforeend', MenuContent);
+
+document.querySelectorAll('.buttoncontainer, .stamp').forEach(btn => {
+  btn.addEventListener('mouseover', () => {
+    let sound = new Audio('./sounds/buttonhover.ogg');
+    sound.play();
+  });
+});
+
+document.querySelectorAll('.buttoncontainer, .stamp').forEach(btn => {
+  btn.addEventListener('mousedown', () => {
+    let sound = new Audio('./sounds/buttonselect.ogg');
+    sound.play();
+  });
+});
