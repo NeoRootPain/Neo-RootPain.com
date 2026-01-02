85377e1d36e6dc2e3a936846d017819ed2dbede3
June Santos <91368271+NeoRootPain@users.noreply.github.com>
2026-01-01 21:23:23 -0300

    Add files via upload

--- js/headerguestbook.js
+++ js/headerguestbook.js
@@ -1,7 +1,7 @@
 let HeaderContent = `
 
 <img src="./images/logos/homepagelogo.png" class="logo" style='right:0px;left:0px;bottom:0px;top:0px;'>
-<img src="./images/logos/bigwhiskeyguestbook.gif" style="position:absolute;left:130px;">
+<img src="./images/logos/bigwhiskeylehatt.gif" style="position:absolute;left:0px;">
 
 `;
 document.querySelector(".headerinnercontainer").insertAdjacentHTML("beforeend", HeaderContent);
