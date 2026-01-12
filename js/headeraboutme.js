d443298f8300f2ef31052b3d0398f7186ec31f56
June Santos <91368271+NeoRootPain@users.noreply.github.com>
2026-01-12 19:26:51 -0300

    Add files via upload

--- js/headeraboutme.js
+++ js/headeraboutme.js
@@ -1,7 +1,7 @@
 let HeaderContent = `
 
 <a href='https://rootpain.com/index.html'>
-<img src="https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/logos/homepagelogo.png?raw=true" class="logo" style='right:0px;left:0px;bottom:0px;top:0px;'></a>
+<img src="https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/logos/logowhite.webp?raw=true" class="logo" style='right:0px;left:0px;bottom:0px;top:0px;'></a>
 
 <img src="https://github.com/NeoRootPain/Neo-RootPain.com/blob/main/images/logos/bigwhiskeyaboutme.gif?raw=true" style="position:absolute;right:0px;">
 
